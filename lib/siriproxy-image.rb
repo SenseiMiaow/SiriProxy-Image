8d4d3915a1a9d772b76bf1aafbfe7d74918bbad8
DJXFMA <djxfma@gmail.com>
2013-02-12 14:52:52 +0100

    Display multiple images on searched value

--- lib/siriproxy-image.rb
+++ lib/siriproxy-image.rb
@@ -13,20 +13,29 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 	def initialize(config)
 	end
 	
-	# Image search
+	# Search images
 	listen_for /(?:(?:image)|(?:show)) (.*)/i do |search|
+		imgUrl = ""
 		strSearch = CGI.escape(search)
-		imgUrl = HTTParty.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=large&q=#{strSearch}").parsed_response['responseData']['results'][0]['unescapedUrl']
-		say "Searched image:"
+		result = HTTParty.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=large&q=#{strSearch}").parsed_response['responseData']['results']
 		
-		object = SiriAddViews.new
-		object.make_root(last_ref_id)
-		answer = SiriAnswer.new("Image: \"#{search}\"", [
-			SiriAnswerLine.new('logo', imgUrl)
-		])
-		object.views << SiriAnswerSnippet.new([answer])
-		send_object object
-
+		say "Searched images:"
+		
+		#result(s)
+		result.each do |item|
+			imgUrl = item["unescapedUrl"]
+			
+			#image
+			object = SiriAddViews.new
+			object.make_root(last_ref_id)
+			answer = SiriAnswer.new("Image: \"#{search}\"", [
+				SiriAnswerLine.new('logo', imgUrl)
+			])
+			object.views << SiriAnswerSnippet.new([answer])
+			send_object object
+			
+		end
+		
 		request_completed
 	end
 	
