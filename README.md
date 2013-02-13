d81fcdd49808f5662d937234b7850d7a54249c2e
DJXFMA <djxfma@gmail.com>
2013-02-13 09:07:20 +0100

    Update code for 9gag json not working with pages

--- README.md
+++ README.md
@@ -11,13 +11,15 @@ You are free to use, modify, and redistribute this gem as long as you give prope
 
 Installation
 ------------
-To install SiriProxy-Image, add the following to your Siri Proxy config.yml file (~/.siriproxy/config.yml):
+To install SiriProxy-Image, you have to install a gem because of 9gag "rvmsudo gem install hpricot" then add the following to your Siri Proxy config.yml file (~/.siriproxy/config.yml):
 
     - name: 'Image'
       git: 'git://github.com/DJXFMA/SiriProxy-Image.git'
       response_title: 'images for my Master'    #Response comes after: Show! cookie monster "images for my master"
       max_results: 5                            #Max results to display each response
 
+Once saved also "rvmsudo bundle update" and maybe "rvmsudo siriproxy update".
+	  
 Stop Siri Proxy (CTRL-C if it's running in the foreground or `killall siriproxy` if it's running in the background)
 
 Update Siri Proxy (`siriproxy update`)
--- lib/siriproxy-image.rb
+++ lib/siriproxy-image.rb
@@ -2,6 +2,8 @@
 require 'cgi'
 require 'json'
 require 'httparty'
+require 'hpricot'
+require 'open-uri'
 
 #######
 # This is a Image plugin.
@@ -14,7 +16,7 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 	def initialize(config)
 		@lastSearch = ""
 		@start = 0
-		@page = 1
+		@page = 1000
 		@max = config["max_results"]
 		@responseTitle = config["response_title"]
 	end
@@ -26,7 +28,7 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 		
 		#more
 		if(search == "more")
-			@page = @page+1
+			@page = @page-1
 			@start = @start+5
 			search = @lastSearch
 			more = " more"
@@ -34,50 +36,73 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 		elsif(search == "nine gag")
 			search = "9gag"
 			@start = 0
-			@page = 1
+			@page = 1000
 			@lastSearch = search
 			more = ""
 		#default
 		else
 			@start = 0
-			@page = 1
+			@page = 1000
 			@lastSearch = search
 			more = ""
 		end
 		
+		#say
+		say "Show!#{more} \"#{search}\" #{@responseTitle}"
+		
 		#9gag
 		if(search == "9gag")
-			result = HTTParty.get("http://9gag.com/hot/index/page/#{@page}?view=json")
-			parsed = JSON.parse(result)
-			result = parsed["items"]
+			#search images
+			images = ""
+			@doc = Hpricot(open("http://9gag.com/hot/#{@page}"))
+			@doc.search("/html/body//img").each do |img|
+				images = images + img.to_s
+			end
+			
+			#display images
+			imgDoc = Hpricot(images)
+			imgDoc.search("//img").each do |image|
+				if(image[:src] =~ /(.*)photo(.*)/)
+					
+					#image
+					object = SiriAddViews.new
+					object.make_root(last_ref_id)
+					answer = SiriAnswer.new("Show!#{more}: \"#{search}\"", [
+						SiriAnswerLine.new('logo', "#{image[:src]}")
+					])
+					object.views << SiriAnswerSnippet.new([answer])
+					send_object object
+					
+				end
+			end
+			
 		#default
 		else
 			strSearch = CGI.escape(search)
 			result = HTTParty.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=medium,large&start=#{@start}&rsz=#{@max}&q=#{strSearch}").parsed_response['responseData']['results']
-		end
-		
-		say "Show!#{more} \"#{search}\" #{@responseTitle}"
-		
-		#result(s)
-		imgUrl = ""
-		result.each do |item|
-			#9gag
-			if(search == "9gag")
-				imgUrl = item
-			else
-				imgUrl = item["unescapedUrl"]
+			
+			#result(s)
+			imgUrl = ""
+			result.each do |item|
+				#9gag
+				if(search == "9gag")
+					imgUrl = item
+				else
+					imgUrl = item["unescapedUrl"]
+				end
+				
+				#image
+				object = SiriAddViews.new
+				object.make_root(last_ref_id)
+				answer = SiriAnswer.new("Show!#{more}: \"#{search}\"", [
+					SiriAnswerLine.new('logo', imgUrl)
+				])
+				object.views << SiriAnswerSnippet.new([answer])
+				send_object object
+				
 			end
-			
-			#image
-			object = SiriAddViews.new
-			object.make_root(last_ref_id)
-			answer = SiriAnswer.new("Show!#{more}: \"#{search}\"", [
-				SiriAnswerLine.new('logo', imgUrl)
-			])
-			object.views << SiriAnswerSnippet.new([answer])
-			send_object object
-			
 		end
+
 		
 		request_completed
 	end
