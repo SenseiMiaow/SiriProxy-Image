ffc771173ef70a13a6eaf97cfcdf12c04cc09e0a
DJXFMA <djxfma@gmail.com>
2013-02-14 11:17:48 +0100

    Update object handling; starts on top instead of last picture loading

--- lib/siriproxy-image.rb
+++ lib/siriproxy-image.rb
@@ -5,25 +5,46 @@ require 'nokogiri'
 require 'open-uri'
 
 #######
-# This is a Image plugin.
-# it will display a Image with on basis from the search value you have spoken
+# This is a fun Image plugin:
+# -. This plugin will display a Image on basis from the search value you have given like
+#    .- (show/image) (me) "cookie monster" => show me "cookie monster"
+#    .- image me "funny cats"
+#    .- show more or image more
+# -. This plugin will also display 9gag images when you say "show 9gag" or "show me 9gag"
+#    .- show more for next page
 ######
 
 class SiriProxy::Plugin::Image < SiriProxy::Plugin
 	
-	
+	#initialize
 	def initialize(config)
 		@lastSearch = ""
 		@start = 0
 		@page = 1000
-		@max = config["max_results"]
-		@responseTitle = config["response_title"]
+		if(config["max_results"] == "")
+			@max = 5
+		else
+			@max = config["max_results"]
+		end
+		if(config["response_title"] == "")
+			@responseTitle = "images for my Master"
+		else
+			@responseTitle = config["response_title"]
+		end
 	end
 	
 	
 	# Show images
-	listen_for /(?:(?:image)|(?:show))(?: me) (.*)/i do |search|
-		search = search[0, search.length-1]
+	listen_for /(?:(?:image)|(?:show))(?:(?: me)|(?: ))(.*)/i do |search|
+		
+		#search space(s) fix
+		if(search[search.length-1, search.length] == " ")
+			search = search[0, search.length-1]
+		end
+		if(search[0, 1] == " ")
+			search = search[1, search.length]
+		end
+		
 		
 		#more
 		if(search == "more")
@@ -46,49 +67,59 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 			more = ""
 		end
 		
+		
 		#say
 		say "Show!#{more} \"#{search}\" #{@responseTitle}"
 		
+		
 		#9gag
 		if(search == "9gag")
+			
+			#start object
+			object = SiriAddViews.new
+			object.make_root(last_ref_id)
 			
 			#display images
 			doc = Nokogiri::HTML(open("http://9gag.com/hot/#{@page}"))
 			doc.xpath("/html/body//img[@src[contains(.,'photo')]]/@src[1]").each do |image|
 				
-				#image
-				object = SiriAddViews.new
-				object.make_root(last_ref_id)
+				#fill object with an image
 				answer = SiriAnswer.new("Show!#{more}: \"#{search}\"", [
 					SiriAnswerLine.new('logo', "#{image}")
 				])
 				object.views << SiriAnswerSnippet.new([answer])
-				send_object object
 				
 			end
 			
+			#send object
+			send_object object
+		
+		
 		#default
 		else
-			strSearch = CGI.escape(search)
-			result = HTTParty.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=medium,large&start=#{@start}&rsz=#{@max}&q=#{strSearch}").parsed_response['responseData']['results']
+			
+			#start object
+			object = SiriAddViews.new
+			object.make_root(last_ref_id)
 			
 			#result(s)
-			imgUrl = ""
+			strSearch = CGI.escape(search)
+			result = HTTParty.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=medium,large&start=#{@start}&rsz=#{@max}&q=#{strSearch}").parsed_response["responseData"]["results"]
 			result.each do |item|
-				imgUrl = item["unescapedUrl"]
 				
-				#image
-				object = SiriAddViews.new
-				object.make_root(last_ref_id)
+				#fill object with an image
 				answer = SiriAnswer.new("Show!#{more}: \"#{search}\"", [
-					SiriAnswerLine.new('logo', imgUrl)
+					SiriAnswerLine.new('logo', item["unescapedUrl"])
 				])
 				object.views << SiriAnswerSnippet.new([answer])
-				send_object object
 				
 			end
+			
+			#send object
+			send_object object
+			
 		end
-
+		
 		
 		request_completed
 	end
